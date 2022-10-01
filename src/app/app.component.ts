ecac6aea9c31988c072134f8b1b31ff69a12c4a9
Brady Kelly <brady.kelly@lexisnexis.co.za>
2022-10-01 07:45:47 +0200

    Added auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { APIService, Restaurant } from "./API.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'amplify-app';
+export class AppComponent implements OnInit, OnDestroy {
+  private subscription: Subscription | null = null;
+  title = "amplify-app";
+  public createForm: FormGroup;
+  restaurants: Restaurant[] = [];
+
+  constructor(private api: APIService, private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      name: ["", Validators.required],
+      description: ["", Validators.required],
+      city: ["", Validators.required],
+    });
+  }
+
+  async ngOnInit() {
+    this.api.ListRestaurants().then((event) => {
+      this.restaurants = event.items as Restaurant[];
+    });
+
+    this.subscription = <Subscription>(
+      this.api.OnCreateRestaurantListener.subscribe((event: any) => {
+        const newRestaurant = event.value.data.onCreateRestaurant;
+        this.restaurants = [newRestaurant, ...this.restaurants];
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = null;
+  }
+
+  public onCreate(restaurant: Restaurant) {
+    this.api
+      .CreateRestaurant(restaurant)
+      .then((event) => {
+        console.log("item created!");
+        this.createForm.reset();
+      })
+      .catch((e) => {
+        console.log("error creating restaurant...", e);
+      });
+  }
 }
